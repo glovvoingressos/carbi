3da5ab31f4e25976d568ae94923e4bb244df29d3
glovvoingressos <glovvotickets@gmail.com>
2026-07-05 16:38:23 -0300

    fix: revert to working structure with contrast improvements

--- src/components/marketplace/ListingForm.tsx
+++ src/components/marketplace/ListingForm.tsx
@@ -1284,7 +1284,6 @@ export default function ListingForm() {
             </motion.div>
             )}
         </AnimatePresence>
-      </div>
 
         {success ? (
           <div className="bg-[#E9FFF2] rounded-[25px] border border-[#16855C]/20 p-6 shadow-sm text-center">
@@ -1341,6 +1340,8 @@ export default function ListingForm() {
             </button>
           </div>
         ) : null}
+      </div>
+
 
     </motion.div>
   )
