6d16e1c2f70238f77c8b1185e7d0227c2141b24f
glovvoingressos <glovvotickets@gmail.com>
2026-07-21 16:53:57 -0300

    fix: force-dynamic on anunciar-carro page

--- src/app/anunciar-carro/page.tsx
+++ src/app/anunciar-carro/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from 'next/navigation'
 import { getSupabaseServerClientWithCookies } from '@/lib/supabase-server'
 
+export const dynamic = 'force-dynamic'
+
 export default async function AnunciarCarroPage() {
   const supabase = await getSupabaseServerClientWithCookies()
   const { data: { user } } = await supabase.auth.getUser()
